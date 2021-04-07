0c4c5813a0fc52b52c8f185c6985ba0580e4c247
Alexander <greenwald80@gmail.com>
2021-04-08 00:53:15 +0300

    chat using eventsource

--- server/eventsource.js
+++ server/eventsource.js
@@ -9,10 +9,14 @@ app.use(cors());
 app.use(express.json());
 
 //get messages
-app.get("/get-messages", (req, res) => {
-  emitter.once("newMessage", (message) => {
-    console.log(message);
-    res.json(message);
+app.get("/connect", (req, res) => {
+  res.writeHead(200, {
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+  });
+  emitter.on("newMessage", (message) => {
+    res.write(`data: ${JSON.stringify(message)} \n\n`);
   });
 });
 
